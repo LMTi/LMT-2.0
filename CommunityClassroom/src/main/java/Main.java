cc26ec577fe1f0cb79270350e83f0ffc09723ccf
LMT <lamotiw@gmail.com>
2026-01-06 20:33:55 +0530

    more exercise added

--- CommunityClassroom/src/main/java/Main.java
+++ CommunityClassroom/src/main/java/Main.java
@@ -8,20 +8,26 @@ public class Main {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         int res = calc.add(2,3);
-        System.out.println("2 + 3 is equal to "+calc);
-        //List interface iorder maintained
+        System.out.println("2 + 3 is equal to "+res);
+        //List interface insertion order maintained
         List<Integer> myList = new ArrayList<>();
         myList.add(2);
         myList.add(4);
         myList.add(44);
         myList.add(5);
+        myList.add(4);
+        System.out.println("List order");
         for (Integer i:myList) {
             System.out.println(i);
         }
 
         // Set interface
         Set<Integer> mySet = new HashSet<Integer>();
-
+        mySet.addAll(myList);
+        System.out.println("Set order");
+        for (Integer i:mySet) {
+            System.out.println(i);
+        }
 
     }
 }
